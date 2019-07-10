af7ff416bb370ca0252cbf01b1a1a5000cf05569
M. Afsori <afsoryafsor@gmail.com>
2019-07-10 19:01:18 +0700

    Menambahkan File Website Portfolio

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//event pada saat link di klik
+$('page-scroll').on('click', function () {
+
+    // ambil isi href
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('.page-scroll').on('click', function (e) {
+
+//     var tujuan = $(this).attr('href');
+
+//     var elemenTujuan = $(tujuan);
+
+//     $('html , body').animate({
+//         scrollTop: elemenTujuan.offset().top - 50
+//     });
+
+//     e.preventDefault();
+// });
